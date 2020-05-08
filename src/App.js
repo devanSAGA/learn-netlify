2825ffc1d1a086049ccf430abe53a58c6dddadad
Devansh Purohit <devanshpurohit@Devanshs-MacBook-Pro.local>
2020-05-08 09:34:04 +0530

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This build is deployed from the master branch.
         </p>
         <a
           className="App-link"
