3739219e620c767d30772b2539a7f02acc3f4aa4
Devansh Purohit <devanshpurohit@Devanshs-MacBook-Pro.local>
2020-05-08 09:39:18 +0530

    Update App.js file [from second-feature-branch]

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This build is deployed from the develop branch.
+          This build is deployed from the second feature branch.
         </p>
         <a
           className="App-link"
