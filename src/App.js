92cdde2453700a89620a22124615c7946548d464
Devansh Purohit <devanshpurohit@Devanshs-MacBook-Pro.local>
2020-05-08 09:35:06 +0530

    Update App.js file [from develop]

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This build is deployed from the master branch.
+          This build is deployed from the develop branch.
         </p>
         <a
           className="App-link"
